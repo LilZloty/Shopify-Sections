74cf8f7c8a280cc68ae616c2bb4d8ce45cd91b58
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2021-11-18 18:21:59 -0600

    Delete shopify.config.js

--- shopify.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    locale: 'en-us',
-    storeDomain: 'zlotyecomdev.myshopify.com',
-    storefrontToken: '644a3f93b79bfa871dbc468fdba45090',
-    graphqlApiVersion: 'unstable',
-};
